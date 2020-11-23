ba1aa02d2ed5b3f3d1153feee5317a5007f397ca
darrickpang <dkypang@yahoo.com>
2020-11-22 17:17:58 -0800

    user authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    user: {
+      id: null,
+      name: "",
+      age: "",
+    }
+  }
+
+  componentDidMount(){
+    //student_token
+    if(localStorage.token){  
+      fetch('http://localhost:3000/user_persist',{
+      headers: {
+        "Authorization": `Bearer ${localStorage.token}`
+      }
+      })
+      .then(res => res.json())
+      .then(json => this.userAuthResponse(json))
+    }
+  }
+
+  userAuthResponse = (json) => {
+    if (json.user){
+      localStorage.token = json.token
+      this.setState({
+        student: {
+          id: json.student.data.attributes.id,
+          name: json.student.data.attributes.name,
+          age: json.student.data.attributes.age
+        },
+        token: json.token
+      }, () => this.props.history.push('/user_main'))
+    }
+  }
+
+  userLogin = ({name, password}) => {
+    let user = {
+      name: name,
+      password: password
+    }
+
+    fetch('http://localhost:3000/user_login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(student)
+    })
+    .then(res => res.json())
+    .then(json => {
+      if (!json.error){
+        this.userAuthResponse(json)
+      } else {
+        alert(json.error)
+      }
+    })
+  }
+
+  userSignUp = ({name, password, age}) => {
+    let newUser = {
+      name: name,
+      password: password,
+      age: age
+    }
+    
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+    .then(res => res.json())
+    .then(json => {
+      if (!json.error) {
+        this.userAuthResponse(json)
+      } else {
+        alert(json.error)
+      }
+    })
+  }
+
+  renderUserLogin = () => {
+    return <StudentLoginSignUp login={true} userLogin={this.userLogin}/>
+  }
+
+  renderUserSignUp = () => {
+    return <StudentLoginSignUp login={false} studentSignUp={this.studentSignUp}/>
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
