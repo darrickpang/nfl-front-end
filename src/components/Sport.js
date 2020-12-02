84468066f32d3bd6279f8528bb55c61ddfb05707
darrickpang <dkypang@yahoo.com>
2020-12-02 14:36:17 -0800

    sports stuff

--- src/components/Sport.js
+++ src/components/Sport.js
@@ -3,12 +3,19 @@ import {  withRouter, BrowserRouter as Router, Route } from 'react-router-dom';
 import { Button } from 'reactstrap';
 
 class Sport extends React.Component {
- 
+    getScores = () => {
+        fetch('http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard')
+    }
+
+    getNews = () => {
+        fetch('http://site.api.espn.com/apis/site/v2/sports/football/nfl/news')
+    }
 
     render(){
         return(
             <div>
                 Sport.js
+                {this.getScores()}
             </div>
         )
     }
