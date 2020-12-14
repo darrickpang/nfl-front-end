265bd7f4f8157e524efa26e322a09521b0a09461
darrickpang <dkypang@yahoo.com>
2020-12-13 20:08:39 -0800

    no need nav

--- src/containers/UserMainContent.js
+++ src/containers/UserMainContent.js
@@ -50,16 +50,13 @@ class UserMainContent extends React.Component {
 
     render(){
         return(
-            <router>
-                <div className="main-page">
-                    {this.renderUserInfo()}
-                    {this.renderLogout()} 
-                    {this.renderSportNews()}
-                    {this.renderSportScores()}
-                    {this.renderTeamData()}
-                </div> 
-            </router>
-            
+            <div className="main-page">
+                {this.renderUserInfo()}
+                {this.renderLogout()} 
+                {this.renderSportNews()}
+                {this.renderSportScores()}
+                {this.renderTeamData()}
+            </div> 
         )
     }
 }
