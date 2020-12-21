a359cbc340eeb33658648f9b31633bc820f54006
darrickpang <dkypang@yahoo.com>
2020-12-20 18:52:19 -0800

    more nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,9 @@ class NavBar extends React.Component {
     render(){
         return(
             <div>
-                
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/scores">Scores</NavLink>
+                <NavLink to="/data">Team Data</NavLink>
             </div>
         )
     }
